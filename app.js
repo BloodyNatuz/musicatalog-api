f6b8ab6d1342579067c0c7713a2becafc9042998
Nathuz <nathuznoepupin@gmail.com>
2024-01-16 07:06:52 +0100

    🍱 Set public folder

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const port = 3000;
 app.set('view engine', 'hbs');
 app.set('views', './views/')
 
+// Setting the public file
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     console.log("Home page get a /GET request.")
     res.render('index', {
         sujet: 'Accueil',
+        styleLink: "/style/style.css"
     });
 });
 
